865404bc90c2d2139f75a32b8cc93735c1048855
surendramohanty1991 <62804059+surendramohanty1991@users.noreply.github.com>
2020-09-21 22:30:02 +0530

    Add files via upload

---
+++ rest_api.py
@@ -0,0 +1,27 @@
+import json
+class RestAPI:
+
+    def __init__(self, database=None):
+        self.database = database
+
+    def get(self, url, payload=None):
+        if payload == None:
+            return json.dumps({"users": []})
+        results = []
+        for u in self.database["users"]:
+            for p in json.loads(payload)["users"]:
+                if((u["name"]) ==p):
+                    results.append(u)
+
+        return json.dumps({"users": results})
+
+    def post(self, url, payload=None):
+        if url == "/add":
+            str_user = json.loads(payload)
+            user = {"name": str_user["user"],
+                    "owes": {},
+                    "owed_by": {},
+                    "balance": 0.0
+                    }
+            self.database[str_user["user"]] = user
+            return json.dumps(user)
